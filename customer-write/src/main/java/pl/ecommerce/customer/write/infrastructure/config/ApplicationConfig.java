eed9ee57942dea778e0bdfebdd3b3d897089d66f
jakub.wszola <jakub.wszola@asseco.pl>
2025-04-19 19:43:04 +0200

    test ci

--- customer-write/src/main/java/pl/ecommerce/customer/write/infrastructure/config/ApplicationConfig.java
+++ customer-write/src/main/java/pl/ecommerce/customer/write/infrastructure/config/ApplicationConfig.java
@@ -1,34 +1,15 @@
 package pl.ecommerce.customer.write.infrastructure.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.observation.aop.ObservedAspect;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.CommonErrorHandler;
 import pl.ecommerce.commons.kafka.TopicsProvider;
-//import pl.ecommerce.commons.kafka.dlq.DlqConfig;
 
 import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-
-	private final KafkaProperties kafkaProperties;
-
-//	@Bean
-//	public ObjectMapper objectMapper() {
-//		ObjectMapper mapper = new ObjectMapper();
-//		mapper.registerModule(new JavaTimeModule());
-//		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-//		return mapper;
-//	}
 
 	@Bean
 	public TopicsProvider topicsProvider() {
@@ -49,13 +30,4 @@ public class ApplicationConfig {
 		));
 		return provider;
 	}
-
-//	@Bean
-//	public CommonErrorHandler kafkaErrorHandler(KafkaTemplate<String, String> kafkaTemplate,
-//												ObjectMapper objectMapper) {
-//		return new DlqConfig(
-//				kafkaProperties,
-//				objectMapper,
-//				kafkaTemplate).deadLetterErrorHandler();
-//	}
 }
