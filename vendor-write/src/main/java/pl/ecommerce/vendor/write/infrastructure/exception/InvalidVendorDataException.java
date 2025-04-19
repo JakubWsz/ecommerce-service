cc8bf71161d86d5e9ea8acc435e913b349a5d99c
jakub.wszola <jakub.wszola@asseco.pl>
2025-04-19 22:05:49 +0200

    fix ci

--- vendor-write/src/main/java/pl/ecommerce/vendor/write/infrastructure/exception/InvalidVendorDataException.java
+++
@@ -1,8 +0,0 @@
-package pl.ecommerce.vendor.write.infrastructure.exception;
-
-public class InvalidVendorDataException extends RuntimeException {
-
-	public InvalidVendorDataException(String message) {
-		super(message);
-	}
-}
